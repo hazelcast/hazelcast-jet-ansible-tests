c0c157e576544cacd0e6f0b87a24afea108074ff
Ali Gurbuz <ali@hazelcast.com>
2018-09-06 19:54:49 +0300

    add jdbc test

--- jdbc-test/src/test/java/com/hazelcast/jet/tests/jdbc/JdbcTest.java
+++ jdbc-test/src/test/java/com/hazelcast/jet/tests/jdbc/JdbcTest.java
@@ -16,5 +16,178 @@
 
 package com.hazelcast.jet.tests.jdbc;
 
+import com.hazelcast.core.IQueue;
+import com.hazelcast.jet.Jet;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Job;
+import com.hazelcast.jet.Util;
+import com.hazelcast.jet.config.JobConfig;
+import com.hazelcast.jet.core.Processor.Context;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sink;
+import com.hazelcast.jet.pipeline.SinkBuilder;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.SourceBuilder;
+import com.hazelcast.jet.pipeline.SourceBuilder.SourceBuffer;
+import com.hazelcast.jet.pipeline.Sources;
+import com.hazelcast.jet.pipeline.StreamSource;
+import com.hazelcast.jet.server.JetBootstrap;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Map.Entry;
+
+import static com.hazelcast.jet.core.JobStatus.FAILED;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(JUnit4.class)
 public class JdbcTest {
+
+    private static final String QUEUE_NAME = "queue";
+    private static final int PERSON_COUNT = 10_000;
+
+
+    private JetInstance jet;
+    private String connectionUrl;
+    private long durationInMillis;
+
+    public static void main(String[] args) {
+        JUnitCore.main(JdbcTest.class.getName());
+    }
+
+    @Before
+    public void setup() throws SQLException {
+        Jet.newJetInstance();
+        Jet.newJetInstance();
+
+        connectionUrl = System.getProperty("connectionUrl",
+                "jdbc:mysql://localhost/soak-test?user=root&password=soak-test");
+        durationInMillis = MINUTES.toMillis(Integer.parseInt(System.getProperty("durationInMinutes", "1")));
+        jet = JetBootstrap.getInstance();
+
+        createAndFillTable();
+    }
+
+    @After
+    public void cleanup() {
+        if (jet != null) {
+            jet.shutdown();
+        }
+    }
+
+    @Test
+    public void test() throws Exception {
+        JobConfig jobConfig = new JobConfig().addClass(JdbcTest.class);
+
+        Sink<String> sink = SinkBuilder
+                .sinkBuilder("queueSink", c -> c.jetInstance().getHazelcastInstance().getQueue(QUEUE_NAME))
+                .<String>receiveFn(IQueue::offer)
+                .preferredLocalParallelism(1)
+                .build();
+
+        StreamSource<Entry<Long, String>> source = SourceBuilder
+                .stream("queueSource", QueueSource::new)
+                .fillBufferFn(QueueSource::addToBuffer)
+                .distributed(1)
+                .build();
+
+
+        Pipeline p1 = Pipeline.create();
+        p1.drawFrom(source)
+          .drainTo(Sinks.jdbc("INSERT INTO PERSON_ALL(id, name) VALUES(?, ?)", connectionUrl,
+                  (stmt, entry) -> {
+                      stmt.setLong(1, entry.getKey());
+                      stmt.setString(2, entry.getValue());
+                  }
+          ));
+
+        Pipeline p2 = Pipeline.create();
+        p2.drawFrom(Sources.jdbc(connectionUrl, "select * from PERSON",
+                resultSet -> resultSet.getString(2)))
+          .drainTo(sink);
+
+        Job streamJob = jet.newJob(p1, jobConfig);
+
+        int jobCounter = 0;
+        long begin = System.currentTimeMillis();
+        while (System.currentTimeMillis() - begin < durationInMillis) {
+            jet.newJob(p2, jobConfig).join();
+            assertNotEquals(FAILED, streamJob.getStatus());
+            jobCounter++;
+        }
+
+        assertTableCount(jobCounter * PERSON_COUNT);
+
+    }
+
+    private void assertTableCount(long expectedTableCount) throws SQLException, InterruptedException {
+        IQueue<String> queue = jet.getHazelcastInstance().getQueue(QUEUE_NAME);
+        for (int i = 0; i < 100; i++) {
+            if (queue.size() == 0 && expectedTableCount == getTableCount()) {
+                return;
+            }
+            SECONDS.sleep(1);
+        }
+        throw new AssertionError("Table count (" + getTableCount() + " should be " + expectedTableCount);
+    }
+
+    private void createAndFillTable() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+             Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE PERSON(id int primary key, name varchar(255))");
+            statement.execute("CREATE TABLE PERSON_ALL(id int primary key, name varchar(255))");
+            try (PreparedStatement stmt = connection.prepareStatement("INSERT INTO PERSON(id, name) VALUES(?, ?)")) {
+                for (int i = 1; i <= PERSON_COUNT; i++) {
+                    stmt.setInt(1, i);
+                    stmt.setString(2, "name-" + i);
+                    stmt.executeUpdate();
+                }
+            }
+        }
+    }
+
+    private long getTableCount() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT count(*) from PERSON_ALL");
+            resultSet.next();
+            return resultSet.getLong(1);
+        }
+    }
+
+    static class QueueSource {
+
+        private final IQueue<String> queue;
+        private final int total;
+
+        private long counter ;
+
+        QueueSource(Context context) {
+            queue = context.jetInstance().getHazelcastInstance().getQueue(QUEUE_NAME);
+            total = context.totalParallelism();
+            counter = context.globalProcessorIndex();
+        }
+
+        void addToBuffer(SourceBuffer<Entry<Long, String>> buffer) {
+            String item = queue.poll();
+            if (item != null) {
+                buffer.add(Util.entry(counter, item));
+                counter += total;
+            }
+        }
+
+    }
+
 }

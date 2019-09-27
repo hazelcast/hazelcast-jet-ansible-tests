ceac3fd8419950c8d88c288cb7ff3e5b04e7e1df
Ali <ali@hazelcast.com>
2019-09-27 10:31:26 +0300

    Count socket timeout number and assert that it happens once in an hour (#28)
    * count socket timeout number and assert that it happens once in an hour
    
    * handle undefined-error-code for client

--- s3-test/src/main/java/com/hazelcast/jet/tests/s3/S3WordCountTest.java
+++ s3-test/src/main/java/com/hazelcast/jet/tests/s3/S3WordCountTest.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.jet.tests.s3;
 
+import com.hazelcast.client.UndefinedErrorCodeException;
 import com.hazelcast.jet.config.JobConfig;
 import com.hazelcast.jet.function.SupplierEx;
 import com.hazelcast.jet.impl.util.ExceptionUtil;
@@ -34,6 +35,7 @@ import software.amazon.awssdk.services.s3.model.S3Object;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.StringTokenizer;
@@ -53,7 +55,7 @@ public class S3WordCountTest extends AbstractSoakTest {
     private static final int GET_OBJECT_RETRY_COUNT = 30;
     private static final long GET_OBJECT_RETRY_WAIT_TIME = TimeUnit.SECONDS.toNanos(1);
     private static final int S3_CLIENT_CONNECTION_TIMEOUT_SECONDS = 10;
-    private static final int S3_CLIENT_SOCKET_TIMEOUT_MINUTES = 2;
+    private static final int S3_CLIENT_SOCKET_TIMEOUT_MINUTES = 5;
 
     private static final String DEFAULT_BUCKET_NAME = "jet-soak-tests-bucket";
     private static final String RESULTS_PREFIX = "results/";
@@ -95,14 +97,40 @@ public class S3WordCountTest extends AbstractSoakTest {
     protected void test() {
         long begin = System.currentTimeMillis();
         int jobNumber = 0;
+        int socketTimeoutNumber = 0;
         while ((System.currentTimeMillis() - begin) < durationInMillis) {
-            JobConfig jobConfig = new JobConfig();
-            jobConfig.setName("s3-test-" + jobNumber);
-            jet.newJob(pipeline(), jobConfig).join();
-            verify(jobNumber);
-            jobNumber++;
+            try {
+                JobConfig jobConfig = new JobConfig();
+                jobConfig.setName("s3-test-" + jobNumber);
+                jet.newJob(pipeline(), jobConfig).join();
+                verify(jobNumber);
+                jobNumber++;
+            } catch (Throwable e) {
+                if (isSocketTimeoutException(e)) {
+                    logger.warning("Socket timeout ", e);
+                    socketTimeoutNumber++;
+                } else {
+                    throw ExceptionUtil.rethrow(e);
+                }
+            }
         }
-        logger.info(String.format("Total number of jobs finished %d", jobNumber));
+        long thresholdForSocketTimeout = TimeUnit.MILLISECONDS.toHours(durationInMillis) + 1;
+        assertTrue("Socket timeout number is too big",thresholdForSocketTimeout > socketTimeoutNumber);
+        logger.info(String.format("Total number of jobs finished: %d, socketTimeout: %d", jobNumber, socketTimeoutNumber));
+    }
+
+    private boolean isSocketTimeoutException(Throwable e) {
+        if (e instanceof SocketTimeoutException) {
+            return true;
+        }
+        if (e instanceof UndefinedErrorCodeException) {
+            return ((UndefinedErrorCodeException) e).getOriginClassName().equals(SocketTimeoutException.class.getName());
+        }
+        Throwable cause = e.getCause();
+        if (cause != null) {
+            return isSocketTimeoutException(cause);
+        }
+        return false;
     }
 
     private Pipeline pipeline() {

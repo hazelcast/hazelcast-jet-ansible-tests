69453bf5fa7353459a6d630f422e0ee9d8b4605e
Ondrej Lukas <ondrej@hazelcast.com>
2020-03-25 11:20:31 +0100

    Added test for transactional JMS sink

---
+++ snapshot-jms-sink-test/src/main/java/com/hazelcast/jet/tests/snapshot/jmssink/JmsSinkTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.tests.snapshot.jmssink;
+
+import com.hazelcast.function.SupplierEx;
+import com.hazelcast.jet.JetInstance;
+import com.hazelcast.jet.Job;
+import com.hazelcast.jet.config.JobConfig;
+import com.hazelcast.jet.config.ProcessingGuarantee;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sink;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.SourceBuilder;
+import com.hazelcast.jet.pipeline.StreamSource;
+import com.hazelcast.jet.tests.common.AbstractSoakTest;
+import javax.jms.ConnectionFactory;
+import org.apache.activemq.ActiveMQXAConnectionFactory;
+
+import static com.hazelcast.function.FunctionEx.identity;
+import static com.hazelcast.jet.core.JobStatus.FAILED;
+import static com.hazelcast.jet.pipeline.ServiceFactories.sharedService;
+import static com.hazelcast.jet.tests.common.Util.getJobStatusWithRetry;
+import static com.hazelcast.jet.tests.common.Util.sleepMillis;
+import static com.hazelcast.jet.tests.common.Util.sleepMinutes;
+import static com.hazelcast.jet.tests.snapshot.jmssink.JmsSinkTest.JmsFactorySupplier.getConnectionFactory;
+
+public class JmsSinkTest extends AbstractSoakTest {
+
+    public static final String SINK_QUEUE = "JmsSinkTest_sink";
+
+    private static final int DEFAULT_SLEEP_MS_BETWEEN_ITEM = 50;
+    private static final int DEFAULT_SNAPSHOT_INTERVAL = 5000;
+
+    private int sleepMsBetweenItem;
+    private long snapshotIntervalMs;
+    private String brokerURL;
+
+    public static void main(String[] args) throws Exception {
+        new JmsSinkTest().run(args);
+    }
+
+    @Override
+    public void init(JetInstance client) throws Exception {
+        sleepMsBetweenItem = propertyInt("sleepMsBetweenItem", DEFAULT_SLEEP_MS_BETWEEN_ITEM);
+        snapshotIntervalMs = propertyInt("snapshotIntervalMs", DEFAULT_SNAPSHOT_INTERVAL);
+        brokerURL = property("brokerURL", "tcp://localhost:61616");
+    }
+
+    @Override
+    protected boolean runOnBothClusters() {
+        return true;
+    }
+
+    @Override
+    public void test(JetInstance client, String name) throws InterruptedException {
+        JmsSinkVerifier verifier = new JmsSinkVerifier(name, brokerURL, logger);
+        verifier.start();
+
+        try {
+            JobConfig jobConfig = new JobConfig();
+            jobConfig.setName(name);
+            jobConfig.setSnapshotIntervalMillis(snapshotIntervalMs);
+            jobConfig.setProcessingGuarantee(ProcessingGuarantee.EXACTLY_ONCE);
+            if (name.startsWith(STABLE_CLUSTER)) {
+                jobConfig.addClass(JmsSinkTest.class, JmsSinkVerifier.class);
+            }
+            Job job = client.newJob(pipeline(name), jobConfig);
+
+            try {
+                long begin = System.currentTimeMillis();
+                while (System.currentTimeMillis() - begin < durationInMillis) {
+                    if (getJobStatusWithRetry(job) == FAILED) {
+                        job.join();
+                    }
+                    verifier.checkStatus();
+                    sleepMinutes(1);
+                }
+            } finally {
+                job.cancel();
+            }
+        } finally {
+            verifier.finish();
+        }
+    }
+
+    @Override
+    protected void teardown(Throwable t) throws Exception {
+    }
+
+    private Pipeline pipeline(String clusterName) {
+        int sleep = sleepMsBetweenItem;
+
+        Pipeline pipeline = Pipeline.create();
+
+        StreamSource<Long> source = SourceBuilder
+                .stream("srcForJmsSink", procCtx -> new long[1])
+                .<Long>fillBufferFn((ctx, buf) -> {
+                    buf.add(ctx[0]++);
+                    sleepMillis(sleep);
+                })
+                .createSnapshotFn(ctx -> ctx[0])
+                .restoreSnapshotFn((ctx, state) -> ctx[0] = state.get(0))
+                .build();
+
+        Sink<Object> sink = Sinks.jmsQueueBuilder(getConnectionFactory(brokerURL))
+                .destinationName(SINK_QUEUE + clusterName)
+                .exactlyOnce(true)
+                .build();
+
+        pipeline.readFrom(source)
+                .withoutTimestamps()
+                .groupingKey(identity())
+                .filterUsingService(sharedService(ctx -> null), (s, k, v) -> true)
+                .writeTo(sink);
+
+        return pipeline;
+    }
+
+    static class JmsFactorySupplier {
+
+        static SupplierEx<ConnectionFactory> getConnectionFactory(String brokerURL) {
+            return () -> new ActiveMQXAConnectionFactory(brokerURL);
+        }
+    }
+}
---
+++ snapshot-jms-sink-test/src/main/java/com/hazelcast/jet/tests/snapshot/jmssink/JmsSinkVerifier.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hazelcast.jet.tests.snapshot.jmssink;
+
+import com.hazelcast.logging.ILogger;
+import java.util.PriorityQueue;
+import javax.jms.Connection;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import static com.hazelcast.jet.impl.util.Util.uncheckRun;
+import static com.hazelcast.jet.tests.snapshot.jmssink.JmsSinkTest.SINK_QUEUE;
+
+public class JmsSinkVerifier {
+
+    private static final int SLEEP_AFTER_VERIFICATION_CYCLE_MS = 1000;
+    private static final int ALLOWED_NO_INPUT_MS = 600000;
+    private static final int QUEUE_SIZE_LIMIT = 20_000;
+    private static final int PRINT_LOG_ITEMS = 10_000;
+    private static final int MAX_VERIFY_BULK_SIZE = 2_000;
+
+    private final Thread consumerThread;
+    private final String name;
+    private final ILogger logger;
+    private final String brokerURL;
+    private final String queueName;
+
+    private volatile boolean finished;
+    private volatile Throwable error;
+    private long counter;
+    private final PriorityQueue<Long> verificationQueue = new PriorityQueue<>();
+
+    public JmsSinkVerifier(String name, String brokerURL, ILogger logger) {
+        this.consumerThread = new Thread(() -> uncheckRun(this::run));
+        this.brokerURL = brokerURL;
+        this.queueName = SINK_QUEUE + name;
+        this.name = name;
+        this.logger = logger;
+    }
+
+    private void run() throws Exception {
+        Connection connection = new ActiveMQConnectionFactory(brokerURL).createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        MessageConsumer consumer = session.createConsumer(session.createQueue(queueName));
+
+
+        long lastInputTime = System.currentTimeMillis();
+        while (!finished) {
+            try {
+                int lastBulkSize = 0;
+                for (Message msg; (msg = consumer.receiveNoWait()) != null && lastBulkSize < MAX_VERIFY_BULK_SIZE;
+                        lastBulkSize++) {
+                    verificationQueue.add(Long.valueOf(((TextMessage) msg).getText()));
+                }
+
+                long now = System.currentTimeMillis();
+                if (lastBulkSize == 0) {
+                    if (now - lastInputTime > ALLOWED_NO_INPUT_MS) {
+                        throw new AssertionError(
+                                String.format("[%s] No new data was added during last %s", name, ALLOWED_NO_INPUT_MS));
+                    }
+                } else {
+                    verifyQueue();
+                    lastInputTime = now;
+                }
+                Thread.sleep(SLEEP_AFTER_VERIFICATION_CYCLE_MS);
+            } catch (Throwable e) {
+                logger.severe("[" + name + "] Exception thrown during processing files.", e);
+                error = e;
+                finished = true;
+            }
+        }
+    }
+
+    private void verifyQueue() {
+        // try to verify head of verification queue
+        for (Long peeked; (peeked = verificationQueue.peek()) != null;) {
+            if (peeked > counter) {
+                // the item might arrive later
+                break;
+            } else if (peeked == counter) {
+                if (counter % PRINT_LOG_ITEMS == 0) {
+                    logger.info(String.format("[%s] Processed correctly item %d", name, counter));
+                }
+                // correct head of queue
+                verificationQueue.remove();
+                counter++;
+            } else if (peeked < counter) {
+                // duplicate key
+                throw new AssertionError(
+                        String.format("Duplicate key %d, but counter was %d", peeked, counter));
+            }
+        }
+        if (verificationQueue.size() >= QUEUE_SIZE_LIMIT) {
+            throw new AssertionError(String.format("[%s] Queue size exceeded while waiting for the next "
+                    + "item. Limit=%d, expected next=%d, next in queue: %s, %s, %s, %s, ...",
+                    name, QUEUE_SIZE_LIMIT, counter, verificationQueue.poll(), verificationQueue.poll(),
+                    verificationQueue.poll(), verificationQueue.poll()));
+        }
+    }
+
+    void start() {
+        consumerThread.start();
+    }
+
+    public void finish() throws InterruptedException {
+        finished = true;
+        consumerThread.join();
+    }
+
+    public void checkStatus() {
+        if (error != null) {
+            throw new RuntimeException(error);
+        }
+        if (finished) {
+            throw new RuntimeException("[" + name + "] Verifier is not running");
+        }
+    }
+
+}

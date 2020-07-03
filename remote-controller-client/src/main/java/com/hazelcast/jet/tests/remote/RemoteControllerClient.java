6b36fd89810069b26a703bbf66ea4386b6d18927
Ali Gurbuz <ali@hazelcast.com>
2020-07-03 10:48:47 +0300

    add assertion for member stop start

--- remote-controller-client/src/main/java/com/hazelcast/jet/tests/remote/RemoteControllerClient.java
+++ remote-controller-client/src/main/java/com/hazelcast/jet/tests/remote/RemoteControllerClient.java
@@ -19,6 +19,7 @@ package com.hazelcast.jet.tests.remote;
 import com.google.common.collect.Iterables;
 import com.hazelcast.cluster.Member;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.function.SupplierEx;
 import com.hazelcast.jet.Jet;
 import com.hazelcast.jet.JetInstance;
 import com.hazelcast.logging.ILogger;
@@ -53,6 +54,9 @@ public final class RemoteControllerClient {
     private static final int DEFAULT_PORT = 9701;
     private static final int VERIFICATION_DURATION_GAP = 15;
     private static final int SLEEP_BETWEEN_CLUSTER_RESTART_SECONDS = 30;
+    private static final int ASSERTION_RETRY_COUNT = 30;
+    private static final String JAVA_PROCESSOR_CHECK =
+            "ps aux | grep JetMemberStarter | grep -v grep | grep -v sudo | wc -l";
 
     private static int logCounter;
     private static ILogger logger;
@@ -122,6 +126,7 @@ public final class RemoteControllerClient {
         int port = member.getAddress().getPort();
         call(member, jetHome + "/bin/jet-cluster-admin -a " + host + " -p " + port +
                 " -o shutdown -c jet -P jet-pass");
+        assertClusterShutdown(members);
         sleepSeconds(1);
         members.forEach(m -> uncheckRun(() -> rollLogs(m, jetHome)));
     }
@@ -129,6 +134,7 @@ public final class RemoteControllerClient {
     private static void stop(Member member, String jetHome) throws Exception {
         logger.info("Stopping member[" + member + "]");
         call(member, "sudo initctl stop hazelcast-jet-isolated");
+        assertMemberStopped(member);
         rollLogs(member, jetHome);
         call(member, "rm -rf " + jetHome + "/hot-restart");
     }
@@ -144,17 +150,22 @@ public final class RemoteControllerClient {
     private static void start(Member member) throws Exception {
         logger.info("Starting member[" + member + "]");
         call(member, "sudo initctl start hazelcast-jet-isolated");
+        assertMemberStarted(member);
     }
 
-    private static void call(Member member, String command) throws Exception {
+    private static String call(Member member, String command) throws Exception {
         TTransport transport = new TSocket(member.getAddress().getHost(), DEFAULT_PORT);
         transport.open();
         TProtocol protocol = new TBinaryProtocol(transport);
         RemoteController.Client client = new RemoteController.Client(protocol);
-        String script = "import subprocess\nprocess = subprocess.call(['" + command + "'], shell=True)";
+        String script = "import subprocess\nresult = subprocess.check_output(['" + command + "'], shell=True)";
         Response response = client.executeOnController(null, script, Lang.PYTHON);
-        logger.info("The response of the command [" + command + "]: " + response);
+        byte[] result = response.getResult();
+        String resultString = result == null ? null : new String(result);
+        logger.info("The response of the command [" + command + "]: " +
+                response.success + " - " + response.message + " - " + resultString);
         transport.close();
+        return resultString;
     }
 
     private static void sleepMinutes(int minutes) throws InterruptedException {
@@ -163,5 +174,35 @@ public final class RemoteControllerClient {
 
     private static void sleepSeconds(int seconds) throws InterruptedException {
         SECONDS.sleep(seconds);
+    }
+
+    private static void assertMemberStarted(Member member) throws Exception {
+        assertWithRetry("Start member [" + member + "] assertion failed", () -> {
+            String result = call(member, JAVA_PROCESSOR_CHECK);
+            return result != null && result.trim().equals("0");
+        });
+    }
+
+    private static void assertMemberStopped(Member member) throws Exception {
+        assertWithRetry("Stop member [" + member + "] assertion failed", () -> {
+            String result = call(member, JAVA_PROCESSOR_CHECK);
+            return result != null && result.trim().equals("1");
+        });
+    }
+
+    private static void assertClusterShutdown(List<Member> members) throws Exception {
+        for (Member member : members) {
+            assertMemberStopped(member);
+        }
+    }
+
+    private static void assertWithRetry(String message, SupplierEx<Boolean> runnable) throws Exception {
+        for (int i = 0; i < ASSERTION_RETRY_COUNT; i++) {
+            if (runnable.get()) {
+                return;
+            }
+            sleepSeconds(1);
+        }
+        throw new AssertionError(message);
     }
 }

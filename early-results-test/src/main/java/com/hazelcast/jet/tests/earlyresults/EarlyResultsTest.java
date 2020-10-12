1d2cffeb489556f740a0186af665f2d9191ce599
Ali Gurbuz <ali@hazelcast.com>
2020-10-12 16:07:24 +0300

    checkstyle

--- early-results-test/src/main/java/com/hazelcast/jet/tests/earlyresults/EarlyResultsTest.java
+++ early-results-test/src/main/java/com/hazelcast/jet/tests/earlyresults/EarlyResultsTest.java
@@ -120,7 +120,7 @@ public class EarlyResultsTest extends AbstractSoakTest {
                 assertTrue(result.isEarly());
                 return;
             }
-            if (tickerWindow.start != result.start()){
+            if (tickerWindow.start != result.start()) {
                 logger.severe("Did not receive the final result for the previous window:\n" +
                         "Current result: " + result + "\n" +
                         "Start index of tickerWindow=" + tickerWindow.start +

534b6a9062c62feafeac7d6a2442b1ceb84e17e0
Ali Gurbuz <ali@hazelcast.com>
2020-10-15 17:07:12 +0300

    wait for produced trades to complete before sending another batch

--- stateful-map-test/src/main/java/com/hazelcast/jet/tests/stateful/KafkaTradeProducer.java
+++ stateful-map-test/src/main/java/com/hazelcast/jet/tests/stateful/KafkaTradeProducer.java
@@ -16,49 +16,71 @@
 
 package com.hazelcast.jet.tests.stateful;
 
-import java.util.Properties;
+import com.hazelcast.logging.ILogger;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import static com.hazelcast.jet.impl.util.Util.uncheckRun;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+import static com.hazelcast.jet.impl.util.Util.uncheckCall;
+import static com.hazelcast.jet.tests.stateful.StatefulMapTest.WAIT_TX_TIMEOUT_FACTOR;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.concurrent.locks.LockSupport.parkNanos;
 
 /**
  * It produces events with String key like [type],[id] where type is 0 for start
  * event and 1 for end event. Value is timestamp.
- *
+ * <p>
  * In every cycle it emits "batchCount" times start event + "batchCount" times
  * end event for them; and 1 transaction event (with negative id) which has only
  * start and which will never get the END event.
- *
+ * <p>
  * Ids are increasing by 1 for each start event and for each end event (i.e.
  * there is just one start event with id x and just one end event with id x).
  */
 public class KafkaTradeProducer implements AutoCloseable {
 
+    private static final int PRODUCE_WAIT_TIMEOUT_MILLIS = 10_000;
+
     private final KafkaProducer<String, Long> producer;
     private final String topic;
     private final long nanosBetweenEvents;
     private final int batchCount;
+    private final int txTimeout;
 
     private final Thread producerThread;
     private volatile boolean finished;
+    private volatile Exception exception;
 
     private long txId;
 
-    public KafkaTradeProducer(String broker, String topic, int txPerSeconds, int batchCount) {
-        this.producerThread = new Thread(() -> uncheckRun(this::run));
+    public KafkaTradeProducer(
+            ILogger logger, String broker, String topic, int txPerSeconds, int batchCount, int txTimeout
+    ) {
+        this.producerThread = new Thread(() -> {
+            try {
+                run();
+            } catch (Exception exception) {
+                logger.severe("Exception while producing trades to topic: " + topic, exception);
+                this.exception = exception;
+            }
+        });
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", broker);
         props.setProperty("key.serializer", StringSerializer.class.getName());
         props.setProperty("value.serializer", LongSerializer.class.getName());
         this.producer = new KafkaProducer<>(props);
         this.topic = topic;
-        nanosBetweenEvents = SECONDS.toNanos(1) / txPerSeconds;
         this.batchCount = batchCount;
+        this.txTimeout = txTimeout;
+        this.nanosBetweenEvents = SECONDS.toNanos(1) / txPerSeconds;
     }
 
     @Override
@@ -67,48 +89,64 @@ public class KafkaTradeProducer implements AutoCloseable {
         producer.close();
     }
 
-    private void run() {
+    private void run() throws Exception {
+        List<Future<RecordMetadata>> futureList = new ArrayList<>(batchCount);
         while (!finished) {
             // generate start events
             for (int i = 0; i < batchCount; i++) {
                 long id = txId + i;
-                produce(0, id, id);
+                futureList.add(produce(0, id, id));
                 parkNanos(nanosBetweenEvents);
             }
+            waitForCompleteAndClearList(futureList);
 
             // generate send events
             for (int i = 0; i < batchCount; i++) {
                 long id = txId + i;
-                produce(1, id, id);
+                futureList.add(produce(1, id, id));
                 parkNanos(nanosBetweenEvents);
             }
+            waitForCompleteAndClearList(futureList);
 
             txId += batchCount;
 
             //This adds a transaction event of type START which will never get the END event
             //Eventually the transaction will be evicted and marked as timeout
             //a single tx is produced per batch and txId<0
-            produce(0, -txId, txId);
+            produce(0, -txId, txId).get(PRODUCE_WAIT_TIMEOUT_MILLIS, MILLISECONDS);
             parkNanos(nanosBetweenEvents);
         }
         //this is to advance wm and eventually evict expired transactions
-        produce(-1, Long.MAX_VALUE, Long.MAX_VALUE - 1);
-        parkNanos(nanosBetweenEvents);
+        parkNanos(MILLISECONDS.toNanos(WAIT_TX_TIMEOUT_FACTOR * txTimeout));
+        produce(-1, Long.MAX_VALUE, Long.MAX_VALUE - 1).get(PRODUCE_WAIT_TIMEOUT_MILLIS, MILLISECONDS);
     }
 
     void start() {
         producerThread.start();
     }
 
-    public long finish() throws InterruptedException {
+    public long finish() throws Exception {
+        checkStatus();
         finished = true;
         producerThread.join();
+        checkStatus();
         return txId;
     }
 
-    private void produce(int type, long id, long timestamp) {
+    public void checkStatus() throws Exception {
+        if (exception != null) {
+            throw exception;
+        }
+    }
+
+    private Future<RecordMetadata> produce(int type, long id, long timestamp) {
         String key = type + "," + id;
-        producer.send(new ProducerRecord<>(topic, key, timestamp));
+        return producer.send(new ProducerRecord<>(topic, key, timestamp));
+    }
+
+    private static void waitForCompleteAndClearList(List<Future<RecordMetadata>> futureList) {
+        futureList.forEach(f -> uncheckCall(() -> f.get(PRODUCE_WAIT_TIMEOUT_MILLIS, MILLISECONDS)));
+        futureList.clear();
     }
 
 }

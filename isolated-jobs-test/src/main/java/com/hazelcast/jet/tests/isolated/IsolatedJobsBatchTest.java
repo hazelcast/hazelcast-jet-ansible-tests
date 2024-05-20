50c3a47f51076e1cd0716be4848ef6eedd41b7b8
Patryk Dadas <patryk.dadas@hazelcast.com>
2024-05-20 10:44:02 +0200

    Fix checkstyle + adjust headers to current year

--- isolated-jobs-test/src/main/java/com/hazelcast/jet/tests/isolated/IsolatedJobsBatchTest.java
+++ isolated-jobs-test/src/main/java/com/hazelcast/jet/tests/isolated/IsolatedJobsBatchTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2024, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.jet.tests.isolated;
 
 import com.hazelcast.cluster.Member;
@@ -21,13 +37,13 @@ import static java.util.stream.Collectors.joining;
 
 /**
  * This test requires Enterprise License with Advanced Compute Feature.
- * To run this test locally, you have to use EE version (it can be forced by adding dependency to hazelcast-enterprise in the pom.xml)
- * and license must be set (it can be done in {@link AbstractSoakTest#run(String[])})
+ * To run this test locally, you have to use EE version (it can be forced by adding dependency to hazelcast-enterprise
+ * in the pom.xml) and license must be set (it can be done in {@link AbstractSoakTest#run(String[])})
  */
 public class IsolatedJobsBatchTest extends AbstractSoakTest {
     public static final int DEFAULT_SLEEP_BETWEEN_NEW_JOBS_IN_MINUTES = 1;
-    private static final int LOG_JOB_COUNT_THRESHOLD = 15;
     public static final String BATCH_SINK_PREFIX = "IsolatedJobsTestBatchSink";
+    private static final int LOG_JOB_COUNT_THRESHOLD = 15;
     private int sleepBetweenNewJobsInMinutes;
 
     public static void main(final String[] args) throws Exception {
@@ -36,7 +52,8 @@ public class IsolatedJobsBatchTest extends AbstractSoakTest {
 
     @Override
     protected void init(HazelcastInstance client) throws Exception {
-        sleepBetweenNewJobsInMinutes = propertyInt("sleepBetweenNewJobsInMinutes", DEFAULT_SLEEP_BETWEEN_NEW_JOBS_IN_MINUTES);
+        sleepBetweenNewJobsInMinutes = propertyInt("sleepBetweenNewJobsInMinutes",
+                DEFAULT_SLEEP_BETWEEN_NEW_JOBS_IN_MINUTES);
     }
 
     @Override
--- isolated-jobs-test/src/main/java/com/hazelcast/jet/tests/isolated/IsolatedJobsStreamTest.java
+++ isolated-jobs-test/src/main/java/com/hazelcast/jet/tests/isolated/IsolatedJobsStreamTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2008-2024, Hazelcast, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hazelcast.jet.tests.isolated;
 
 import com.hazelcast.collection.IList;
@@ -28,8 +44,8 @@ import static java.util.stream.Collectors.joining;
 
 /**
  * This test requires Enterprise License with Advanced Compute Feature.
- * To run this test locally, you have to use EE version (it can be forced by adding dependency to hazelcast-enterprise in the pom.xml)
- * and license must be set (it can be done in {@link AbstractSoakTest#run(String[])})
+ * To run this test locally, you have to use EE version (it can be forced by adding dependency to hazelcast-enterprise
+ * in the pom.xml) and license must be set (it can be done in {@link AbstractSoakTest#run(String[])})
  */
 public class IsolatedJobsStreamTest extends AbstractSoakTest {
     private static final int DEFAULT_LOG_VERIFICATION_COUNT_THRESHOLD = 5;
@@ -110,7 +126,8 @@ public class IsolatedJobsStreamTest extends AbstractSoakTest {
             boolean result = uuids.stream()
                     .noneMatch(uuid -> uuid.equals(excludedMember));
             if (!result) {
-                throw new AssertionError("Job (" + jobName + ") was executed on excluded member (" + excludedMember + ")." +
+                throw new AssertionError("Job (" + jobName + ") was executed on excluded member ("
+                        + excludedMember + ")." +
                         "List of members from execution: " + uuids.stream()
                         .map(UUID::toString)
                         .collect(joining(","))
@@ -158,4 +175,3 @@ public class IsolatedJobsStreamTest extends AbstractSoakTest {
                 .build();
     }
 }
-

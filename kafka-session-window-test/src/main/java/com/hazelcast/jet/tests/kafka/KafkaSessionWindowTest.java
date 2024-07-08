3bc31963572606692917f3be4d27b8ea7e2954fe
Patryk Dadas <patryk.dadas@hazelcast.com>
2024-07-08 21:28:17 +0200

    Reduce memory usage for AttachMLModelTest and catch possible issues during cancelling in KafkaSessionWindowTest

--- kafka-session-window-test/src/main/java/com/hazelcast/jet/tests/kafka/KafkaSessionWindowTest.java
+++ kafka-session-window-test/src/main/java/com/hazelcast/jet/tests/kafka/KafkaSessionWindowTest.java
@@ -31,7 +31,9 @@ import com.hazelcast.jet.pipeline.Sinks;
 import com.hazelcast.jet.pipeline.StreamStage;
 import com.hazelcast.jet.tests.common.AbstractJetSoakTest;
 import com.hazelcast.map.IMap;
+
 import java.time.Duration;
+
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -136,8 +138,18 @@ public class KafkaSessionWindowTest extends AbstractJetSoakTest {
             }
         } finally {
             System.out.println("Cancelling jobs..");
-            testJob.cancel();
-            verificationJob.cancel();
+            try {
+                testJob.cancel();
+            } catch (Throwable t) {
+                System.out.println("Cancelling test job failed... " + t.getMessage());
+                t.printStackTrace();
+            }
+            try {
+                verificationJob.cancel();
+            } catch (Throwable t) {
+                System.out.println("Cancelling verification job failed... " + t.getMessage());
+                t.printStackTrace();
+            }
         }
 
     }
